01b4d47285fd99ad2d9a40d7aad1beab1cbfffde
Enemy Editor Team <team@example.local>
2026-09-22 11:43:54 -0500

    chore: initialize enemy editor WPF solution

---
+++ src/EnemyEditor.Wpf/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace EnemyEditor.Wpf;
+
+public partial class App : Application
+{
+}
+
---
+++ src/EnemyEditor.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace EnemyEditor.Wpf;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+
